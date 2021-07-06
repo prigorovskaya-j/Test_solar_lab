268bb97f928106c07cad19725a6b594699836ee4
Юлия <prigorovskaya.j.i.2.18@gmail.com>
2021-07-07 01:06:34 +0300

    Изменена структура. Реализовано добавление в список и вывод списка

--- testing_solar/testing_solar/Program.cs
+++ testing_solar/testing_solar/Program.cs
@@ -1,7 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
 
 namespace testing_solar
 {
+    class NameList 
+    {
+        public string name { get; set; } = "NN";
+        public int DateTime { get; set; } = 04051991;
+        public override string ToString()
+        {
+            return name + " " + DateTime;
+        }
+    }
+    static class Ex
+    {
+        static public void Print(this List<NameList> list)
+        {
+            foreach (var el in list)
+            {
+                Write(el + " " +"\n");
+            }
+            WriteLine();
+        }
+    }
     class Program
     {
         protected static int MenuItem;
@@ -15,48 +38,55 @@ namespace testing_solar
             Console.WriteLine("5 - Удалить день рождения");
             Console.WriteLine("6 - Сохранить в файл");
             Console.WriteLine("7 - Загрузить из файла");
-            //Console.WriteLine("8 - Сортировать");
             Console.WriteLine("0 - Выход");
             MenuItem = Convert.ToInt32(Console.ReadLine());
-           // Console.WriteLine($"MenuItem={MenuItem}");
             return MenuItem;
-        }
-        static void CallMethod()
-        {
-            switch (MenuItem) {
-                case 1:
-                    Console.WriteLine($"Вы выбрали {MenuItem}");
-                    break;
-                case 2:
-                    Console.WriteLine($"Вы выбрали {MenuItem}");
-                    break;
-                case 3:
-                    Console.WriteLine($"Вы выбрали {MenuItem}");
-                    break;
-                case 4:
-                    Console.WriteLine($"Вы выбрали {MenuItem}");
-                    break;
-                case 5:
-                    Console.WriteLine($"Вы выбрали {MenuItem}");
-                    break;
-                case 6:
-                    Console.WriteLine($"Вы выбрали {MenuItem}");
-                    break;
-                case 7:
-                    Console.WriteLine($"Вы выбрали {MenuItem}");
-                    break;
-                case 8:
-                    Console.WriteLine($"Вы выбрали {MenuItem}");
-                    break;
-                default:
-                    Console.WriteLine($"Вы выбрали {MenuItem}");
-                    break;
-            }
         }
         static void Main(string[] args)
         {
-            MenuSelection();
-            CallMethod();
+            List<NameList> list = new List<NameList>();
+            do
+            {
+                MenuSelection();
+                switch (MenuItem)
+                {
+                    case 1:
+                        Console.WriteLine($"Вы выбрали {MenuItem}");
+                        Console.WriteLine("Все дни рождения в списке: ");
+                        list.Print();
+                        break;
+                    case 2:
+                        Console.WriteLine($"Вы выбрали {MenuItem}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Вы выбрали {MenuItem}");
+                        Console.WriteLine("Для добавления в список...");
+                        Console.WriteLine("Введите имя");
+                        string _name = Console.ReadLine();
+                        Console.WriteLine("Введите дату (01011999)");
+                        int _date = Convert.ToInt32(Console.ReadLine());
+                        list.Add(new NameList() { name = _name, DateTime = _date });
+                        break;
+                    case 4:
+                        Console.WriteLine($"Вы выбрали {MenuItem}");
+                        break;
+                    case 5:
+                        Console.WriteLine($"Вы выбрали {MenuItem}");
+                        break;
+                    case 6:
+                        Console.WriteLine($"Вы выбрали {MenuItem}");
+                        break;
+                    case 7:
+                        Console.WriteLine($"Вы выбрали {MenuItem}");
+                        break;
+                    case 8:
+                        Console.WriteLine($"Вы выбрали {MenuItem}");
+                        break;
+                    default:
+                        Console.WriteLine($"Вы выбрали {MenuItem}");
+                        break;
+                }
+            } while (MenuItem != 0);
         }
     }
 }
